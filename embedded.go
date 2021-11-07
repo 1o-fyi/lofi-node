0481797b6cd19e646d1e6c0490f9f8fba62a514e
John S <john@sahhar.io>
2021-11-07 03:41:43 -0800

    all: update embedded config
    go.mod/sum: update to lofi/lib v0.0.11
    
    Signed-off-by: John S <john@sahhar.io>

--- embedded.go
+++ embedded.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"embed"
 	_ "embed"
+	"fmt"
+	"log"
 	"strings"
 
 	"git.sr.ht/~lofi/lib"
@@ -11,27 +14,40 @@ import (
 // this means rebuilding to change the configuration; pros and cons without a doubt.
 // If you know a better way to do this please reach out.
 
-//go:embed cfg/domain.cfg
-var domain string
+var (
+	// go:embed embedded/fs/*
+	embeddedUsers embed.FS
 
-//go:embed cfg/name.cfg
-var name string
+	// domain is the public facing url of
+	// the host. ( e.g foo.com )
+	// No need to prefix with https, however,
+	// you can specify the port if you don't want
+	// to use the default of 443 ( e.g. foo.com:880 )
+	// go:embed embedded/cfg/domain.cfg
+	domain string
 
-//go:embed cfg/apath.cfg
-var apath string
+	// redisdomain is the domain where your redis
+	// instance can be reached, same rules as above apply.
+	//go:embed embedded/cfg/redisDomain.cfg
+	redisDomain string
 
-//go:embed cfg/mode.cfg
-var mode string
-
-//go:embed fs/art/home.b64
-var rawArt []byte
+	//go:embed embedded/art/home.b64
+	rawArt []byte
+)
 
 func init() {
 	rawArt = <-lib.DecodeBase64(rawArt)
-	apath = trimDecode(apath)
-	name = trimDecode(name)
+	redisDomain = trimDecode(redisDomain)
 	domain = trimDecode(domain)
-	mode = strings.TrimSpace(mode)
+	userFiles, err := embeddedUsers.ReadDir("users")
+	if err != nil {
+		panic(fmt.Sprintf("err parsing user file %v", err))
+	}
+
+	for _, user := range userFiles {
+		log.Println(user)
+	}
+
 }
 
 func trimDecode(s string) string {
