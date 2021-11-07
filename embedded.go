7c142a05a19ba8fe071fffd982cd69ad252019e4
john s <john@sahhar.io>
2021-11-07 04:29:34 -0800

    embedded/fix domain (#3)
    * embedded: remove userlist
    
    Signed-off-by: John S <john@sahhar.io>
    
    * embedded: fix domain name parsing

--- embedded.go
+++ embedded.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"embed"
 	_ "embed"
-	"fmt"
-	"log"
-	"strings"
+	"os"
 
 	"git.sr.ht/~lofi/lib"
 )
@@ -15,15 +12,12 @@ import (
 // If you know a better way to do this please reach out.
 
 var (
-	// go:embed embedded/fs/*
-	embeddedUsers embed.FS
-
 	// domain is the public facing url of
 	// the host. ( e.g foo.com )
 	// No need to prefix with https, however,
 	// you can specify the port if you don't want
 	// to use the default of 443 ( e.g. foo.com:880 )
-	// go:embed embedded/cfg/domain.cfg
+	//go:embed embedded/cfg/domain.cfg
 	domain string
 
 	// redisdomain is the domain where your redis
@@ -37,19 +31,10 @@ var (
 
 func init() {
 	rawArt = <-lib.DecodeBase64(rawArt)
-	redisDomain = trimDecode(redisDomain)
-	domain = trimDecode(domain)
-	userFiles, err := embeddedUsers.ReadDir("users")
-	if err != nil {
-		panic(fmt.Sprintf("err parsing user file %v", err))
-	}
-
-	for _, user := range userFiles {
-		log.Println(user)
-	}
-
-}
-
-func trimDecode(s string) string {
-	return string(<-lib.DecodeHex([]byte(strings.TrimSpace(s))))
+	redisDomain = string(<-lib.DecodeHex([]byte(redisDomain)))
+	domain = string(<-lib.DecodeHex([]byte(domain)))
+	os.Stdout.Write([]byte("\n[ lofi-node config ]"))
+	os.Stdout.Write([]byte("\ndomain: " + domain))
+	os.Stdout.Write([]byte("\nredis: " + redisDomain))
+	os.Stdout.Write([]byte("\n"))
 }
